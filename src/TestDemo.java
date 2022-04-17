634129efd9f068c1e1b334c77506edfdb06aef45
影子你陪着我累吗 <1477649017@qq.com>
2022-04-17 12:24:05 +0800

    java第一次尝试提交

---
+++ src/TestDemo.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14776
+ * Date: 2022-04-17
+ * Time: 12:21
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello bit");
+    }
+
+
+
+}
