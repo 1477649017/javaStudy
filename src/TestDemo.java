dca70d523dc3cbebb58d406c3ba856924e29869c
影子你陪着我累吗 <1477649017@qq.com>
2022-04-20 22:44:08 +0800

    数据类型与变量

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -1,52 +1,19 @@
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: 14776
- * Date: 2022-04-19
- * Time: 11:59
- */
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 
 public class TestDemo {
-
     public static void main(String[] args) {
-        char num3 ='a';
-        System.out.println(num3);
+//        字符串转数字
+        String str = "1234";
+        int ret = Integer.parseInt(str);
+        System.out.println(ret);
+//         数字转字符串
+        String str1 = String.valueOf(ret);
+        System.out.println(str1);
+//        字符串+的拼接
+        int a = 10;
+        int b = 20;
+        System.out.println( a + b + "aaaaa");
+        System.out.println("aaaaa" + a + b);
+        System.out.println("aaaaa" + (a + b));
 
-        char num4 ='王';
-        System.out.println(num4);
-
-        char num5 = 97;
-        System.out.println(num5);
-
-        short a =128;
-
-        byte b =(byte) a;
-        System.out.println(a);
-        System.out.println(b);
-    }
-    public static void fun(String[] args) {
-        int a = Integer.MAX_VALUE;
-        int b = Integer.MAX_VALUE + 1;
-        System.out.println(a);
-        System.out.println(b);
-        int c = 10;
-        System.out.println(c);
-
-        //长整型
-        long unm = 10L;//定义长整形后面必须要加上一个L，最好用大写
-        System.out.println("长整形最大值 ：" + Long.MAX_VALUE );
-        System.out.println("长整形最小值 ：" + Long.MIN_VALUE);
-
-        //双精度浮点型
-        double  num1 = 1.2;
-        System.out.println("浮点型最大值：" + Double.MAX_VALUE);
-        System.out.println("浮点型最小值：" + Double.MIN_VALUE);
-        System.out.printf("%.2f\n",num1);
-
-        //单精度浮点型
-        float num2 = 1.2f;
-        System.out.println(num2);
     }
 }
