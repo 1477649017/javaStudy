7a6365fe7b3f2589dc44f02c1bc0b9a22e1da834
影子你陪着我累吗 <1477649017@qq.com>
2022-06-26 17:23:56 +0800

    ArrayList

---
+++ GetPosLegalException.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14776
+ * Date: 2022-06-26
+ * Time: 8:49
+ */
+public class GetPosLegalException extends RuntimeException{
+    public GetPosLegalException(){
+
+    }
+    public GetPosLegalException(String msg){
+        super(msg);
+    }
+}
--- TestDemo220625.java
+++ TestDemo220625.java
@@ -5,7 +5,7 @@
  * Date: 2022-06-25
  * Time: 8:41
  */
-import java.util.Arrays;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,7 +15,9 @@ import java.util.Arrays;
  * Time: 7:47
  */
 
-//模拟一个顺序表接口的实现
+//模拟一个顺序表
+import javax.print.DocFlavor;
+import java.util.*;
 
 class MyArraylist{
     //    以下是数组
@@ -52,17 +54,18 @@ class MyArraylist{
 //    数据结构是一门严谨的学科，所以需要我们去分析好每一种情况
     private void CheckAddPos(int pos){
         if(pos < 0 || pos > this.UsedSize){//当pos == Usedsize时就是尾部插入，但是不能跳着插入，大于就不行了
-            throw new AddPosLegalException("pos不合法！");
+            throw new AddPosLegalException("pos不合法！插入元素过程");
         }
     }
     public void add(int pos, int data) {
-//        首先判断数组是否满了
-        if(isFull()){
-            this.elem = this.elem = Arrays.copyOf(elem,2*elem.length);
-        }
-//        然后判断我们的pos下标是否合法
+//        判断我们的pos下标是否合法
         try{
             CheckAddPos(pos);
+            //        首先判断数组是否满了
+            if(isFull()){
+                this.elem = this.elem = Arrays.copyOf(elem,2*elem.length);
+            }
+
             for(int i = this.UsedSize - 1;i >= pos;i--){
                 this.elem[i+1] = this.elem[i];
             }
@@ -73,24 +76,126 @@ class MyArraylist{
         }
     }
     // 判定是否包含某个元素
-    public boolean contains(int toFind) { return true; }
+    public boolean contains(int toFind) {
+        for(int i = 0;i < this.UsedSize;i++){//不需要判断顺序表是否为空，因为空的时候for循环进不去
+            if(this.elem[i] == toFind){
+                return true;//找到了就返回true
+            }
+        }
+        return false;
+    }
     // 查找某个元素对应的位置
-    public int indexOf(int toFind) { return -1; }
+    public int indexOf(int toFind) {
+        for(int i = 0;i < this.UsedSize;i++){//不需要判断顺序表是否为空，因为空的时候for循环进不去
+            if(this.elem[i] == toFind){
+                return i;//找到了就返回下标i
+            }
+        }
+        return -1;
+    }
     // 获取 pos 位置的元素
-    public int get(int pos) { return -1; }
+    private void CheckGetPos(int pos){
+        if(pos < 0 || pos >= this.UsedSize){
+            throw new GetPosLegalException("pos位置不合法！取元素过程");
+        }
+    }
+    public int get(int pos) {
+//       首先也是判断pos是否合法，不过这个pos的合法条件与add的是不同的
+        try{
+            CheckGetPos(pos);
+        }catch (GetPosLegalException e){
+            e.printStackTrace();
+        }
+        return this.elem[pos];
+    }
     // 给 pos 位置的元素设为 value
-    public void set(int pos, int value) { }
+    public void set(int pos, int value) {
+        //首先检查pos位置的合法性
+        CheckGetPos(pos);//有异常直接扔给JVM
+        this.elem[pos] = value;
+    }
     //删除第一次出现的关键字key
-    public void remove(int toRemove) { }
+    public void remove(int key) {
+        //先找到key的位置
+        int pos = indexOf(key);
+        if(pos == -1){
+            System.out.println("顺序表中没有你想要删除的元素！");
+            return;
+        }
+        for(int i = pos;i < this.UsedSize - 1;i++){
+            this.elem[i] = this.elem[i+1];
+        }
+//      this.elem[UsedSize] = null;如果说数组中是引用类型的话，你删除后最后一个元素需要手动置为空，防止内存泄漏
+        this.UsedSize--;
+    }
     // 获取顺序表长度
-    public int size() { return 0; }
+    public int size() {
+        return this.UsedSize;
+    }
     // 清空顺序表
-    public void clear() { }
+    public void clear() {
+        this.UsedSize = 0;
+    }
 
 
 }
 public class TestDemo220625 {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+//        sout直接输出
+        System.out.println(list);
+//        for循环遍历输出
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+//        foreach循环遍历输出
+        for (Integer tmp:list) {
+            System.out.print(tmp + " ");
+        }
+        System.out.println();
+//        迭代器1
+        Iterator<Integer> it = list.iterator();
+        while(it.hasNext()){
+            System.out.print(it.next() + " ");
+        }
+        System.out.println();
+//        迭代器2
+        ListIterator<Integer> it1 = list.listIterator();
+        while(it1.hasNext()){
+            System.out.print(it1.next() + " ");
+        }
+
+    }
+    public static void main4(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("haha");
+        list1.add("hehe");
+        list1.add("hoho");
+        list1.add("huhu");
+        List<String> ret = list1.subList(1,3);
+        System.out.println(ret);
+        ret.set(0,"haha");
+        System.out.println(list1);
+        System.out.println(ret);
+    }
+    public static void main3(String[] args) {
+        ArrayList<Integer> arrayList1 = new ArrayList<>(15);
+        arrayList1.add(10);
+        System.out.println(arrayList1);
+        ArrayList<Integer> arrayList2 = new ArrayList<>(arrayList1);
+        System.out.println(arrayList2);
+    }
+    public static void main2(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(10);
+        System.out.println(arrayList);
+    }
+    public static void main1(String[] args) {
         MyArraylist myArraylist = new MyArraylist();
         myArraylist.add(1);
         myArraylist.add(2);
@@ -98,6 +203,12 @@ public class TestDemo220625 {
         myArraylist.display();
         myArraylist.add(1,10);
         myArraylist.display();
+        System.out.println(myArraylist.contains(10));
+        System.out.println(myArraylist.get(2));
+        myArraylist.set(0,100);
+        myArraylist.display();
+
+        
 
     }
 }
