8655d0f891331a1b74a97ef8698d82622f915ed8
影子你陪着我累吗 <1477649017@qq.com>
2022-11-15 23:39:55 +0800

    servlet学习

---
+++ servlet/src/main/java/HelloServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14776
+ * Date: 2022-11-15
+ * Time: 20:24
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
+    }
+
+}
