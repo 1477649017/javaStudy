285d3c2726a6b62dfde6267c019d3ef8dc740a70
影子你陪着我累吗 <1477649017@qq.com>
2022-11-18 23:48:13 +0800

    Servlet

--- servlet/src/main/java/HelloServlet.java
+++ servlet/src/main/java/HelloServlet.java
@@ -1,4 +1,5 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,10 +12,19 @@ import java.io.IOException;
  * Date: 2022-11-15
  * Time: 20:24
  */
+
+
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //这个自动生成的调用父类doGet方法的代码一定要注释掉，不然就会出问题
         //super.doGet(req, resp);
+
+        //Tomcat中打印一个日志
+        System.out.println("hello world");
+        //给客户端写回一个响应
+        resp.getWriter().write("hello world");
     }
 
 }
