73d956c6d9c23734bce13a53b886b7e5f644dd87
影子你陪着我累吗 <1477649017@qq.com>
2023-01-12 22:40:36 +0800

    Spring的创建和使用

---
+++ SpringCreate/src/main/java/App.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14776
+ * Date: 2023-01-12
+ * Time: 22:21
+ */
+public class App {
+    //这是一个测试启动类
+    public static void main(String[] args) {
+        //1 获取Spring的上下文对象
+    }
+}
---
+++ SpringCreate/src/main/java/Demo.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 14776
+ * Date: 2023-01-12
+ * Time: 21:53
+ */
+public class Demo {
+    public String func(){
+        return "hello java";
+    }
+}
