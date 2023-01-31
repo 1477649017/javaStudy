72d585944d612baf36394bd3394b0d9cb539a8a4
影子你陪着我累吗 <1477649017@qq.com>
2023-01-31 22:05:46 +0800

    回溯算法题目

--- 算法训练营/src/BacktrackingAlgorithm/DFS/Solution.java
+++ 算法训练营/src/BacktrackingAlgorithm/DFS/Solution.java
@@ -1,36 +1,70 @@
 package BacktrackingAlgorithm.DFS;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//class Solution {
+//    public static List<Integer> path = new ArrayList<>();
+//    public static List<List<Integer>> ret = new ArrayList<>();
+//
+//    public void DFS(int[] nums,int index){
+//        ret.add(new ArrayList<>(path));
+//        //index表示每一次的起始位置
+//        if(index >= nums.length){
+//            //终止条件
+//            return ;
+//        }
+//
+//        for(int i = index;i < nums.length;i++){
+//            path.add(nums[i]);
+//            DFS(nums,i+1);
+//            path.remove(path.size() - 1);
+//        }
+//
+//    }
+//    public List<List<Integer>> subsets(int[] nums) {
+//        DFS(nums,0);
+//        return ret;
+//    }
+//
+//    public static void main(String[] args) {
+//        Solution solution = new Solution();
+//        int[] nums = new int[]{1,2,3};
+//        solution.subsets(nums);
+//        System.out.println(ret);
+//    }
+//}
 
+
+////////////////////////////////////////////////////////////////////////
+import java.util.*;
 class Solution {
     public static List<Integer> path = new ArrayList<>();
     public static List<List<Integer>> ret = new ArrayList<>();
+    public void DFS(int index,int n,int k){
 
-    public void DFS(int[] nums,int index){
-        ret.add(new ArrayList<>(path));
-        //index表示每一次的起始位置
-        if(index >= nums.length){
-            //终止条件
+        if(path.size() == k){
+            ret.add(new ArrayList<>(path));
             return ;
         }
 
-        for(int i = index;i < nums.length;i++){
-            path.add(nums[i]);
-            DFS(nums,i+1);
+        for(int i = index;i <= n;i++){
+            path.add(i);
+            DFS(i+1,n,k);
             path.remove(path.size() - 1);
         }
-        
     }
-    public List<List<Integer>> subsets(int[] nums) {
-        DFS(nums,0);
+    public List<List<Integer>> combine(int n, int k) {
+        DFS(1,n,k);
         return ret;
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = new int[]{1,2,3};
-        solution.subsets(nums);
+        Solution sl = new Solution();
+        sl.combine(1,1);
         System.out.println(ret);
+
+        Map<Integer,Boolean> map = new HashMap<>();
+        
     }
 }
